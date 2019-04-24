6cf01598a79ee62d495590450e11973e178b1979
guojunliang <guo601094>
2019-04-24 18:45:09 +0800

    fix

---
+++ concurrent-coding-service/src/main/java/com/jd/jr/access/TestSingleton.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: TestSingleton
+ * Author:   guojunliang
+ * Date:     2019/4/24 18:44
+ * Description: 测试单利
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jd.jr.access;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈测试单例〉
+ *
+ * @author guojunliang
+ * @create 2019/4/24
+ * @since 1.0.0
+ */
+public class TestSingleton {
+
+}
