df8f42448acbfdc438f1c2f4d25326953c119128
guojunliang <guo601094>
2019-05-15 17:23:07 +0800

    fix

---
+++ concurrent-coding-service/src/main/java/com/jd/jr/access/test/Test.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Test
+ * Author:   guojunliang
+ * Date:     2019/5/13 15:43
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jd.jr.access.test;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author guojunliang
+ * @create 2019/5/13
+ * @since 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        long a = 5;
+        long b = 3;
+        System.out.println((long)Math.ceil(a/(double)b));
+    }
+
+}
